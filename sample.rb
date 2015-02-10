6a586526101e678bdadec5b93eac781e5d3f3d7f
okura <hiroshi.okura@jrits.ricoh.co.jp>
2015-02-10 14:55:37 +0900

    patch to headless

--- sample.rb
+++ sample.rb
@@ -2,6 +2,20 @@ require 'rubygems'
 require 'headless'
 require 'selenium-webdriver'
 
+# monkey patch until close https://github.com/leonid-shevtsov/headless/pull/53
+class Headless
+  class VideoRecorder
+    def start_capture
+      CliUtil.fork_process("#{CliUtil.path_to('ffmpeg')} -y -r #{@frame_rate} -s #{@dimensions} -f x11grab -i :#{@display} -g 600 -vcodec #{@codec} #{@tmp_file_path}", @pid_file_path, @log_file_path)
+      at_exit do
+        exit_status = $!.status if $!.is_a?(SystemExit)
+        stop_and_discard
+        exit exit_status if exit_status
+      end
+    end
+  end
+end
+
 options = {}
 options[:dimensions]            = ENV['DISPLAY_DIMENSIONS'] || Headless::DEFAULT_DISPLAY_DIMENSIONS
 options[:video]                 = {}
